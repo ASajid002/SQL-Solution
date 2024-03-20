ca0767351a7b805a718aec360048f4ead30c50b8
ASajid002 <164238631+ASajid002@users.noreply.github.com>
2024-03-21 01:43:31 +0530

    Update Solution.sql

--- HackerRank SQL Challenges - Solution/Advanced Select/New Companies/Solution.sql
+++ HackerRank SQL Challenges - Solution/Advanced Select/New Companies/Solution.sql
@@ -1,7 +1,3 @@
-/*
-Enter your query here.
-Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
-*/
 SELECT C.COMPANY_CODE,
        C.FOUNDER,
   (SELECT COUNT(DISTINCT LEAD_MANAGER_CODE)
