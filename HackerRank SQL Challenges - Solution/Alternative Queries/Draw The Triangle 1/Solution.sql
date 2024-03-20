d20de3c0418e7493dde9f04da7f11eb656a9d7e2
ASajid002 <164238631+ASajid002@users.noreply.github.com>
2024-03-21 01:35:28 +0530

    Update Solution.sql

--- HackerRank SQL Challenges - Solution/Alternative Queries/Draw The Triangle 1/Solution.sql
+++ HackerRank SQL Challenges - Solution/Alternative Queries/Draw The Triangle 1/Solution.sql
@@ -1,6 +1,19 @@
-/*
-Enter your query here.
-Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
-*/
-SELECT RPAD('*', (21-LEVEL)*2, ' *')
-FROM DUAL CONNECT BY LEVEL <= 20;
+WITH RECURSIVE TRNGL AS (
+    SELECT 
+        20 AS T, 
+        1 AS R, 
+        '* * * * * * * * * * * * * * * * * * * *' AS TRNGL
+    UNION ALL
+    SELECT 
+        20, 
+        R + 1, 
+        SUBSTRING(TRNGL, 1, LENGTH(TRNGL) - 2) AS TRNGL
+    FROM 
+        TRNGL
+    WHERE 
+        T > R
+)
+SELECT TRNGL
+FROM TRNGL
+ORDER BY TRNGL DESC;
+
