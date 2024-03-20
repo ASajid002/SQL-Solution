dca02c3a17e42e00773912bb24c246391b2d9034
ASajid002 <164238631+ASajid002@users.noreply.github.com>
2024-03-21 01:48:43 +0530

    Update Solution.sql

--- HackerRank SQL Challenges - Solution/Advanced Select/Type of Triangle/Solution.sql
+++ HackerRank SQL Challenges - Solution/Advanced Select/Type of Triangle/Solution.sql
@@ -1,16 +1,8 @@
-/*
-Enter your query here.
-Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
-*/
-SELECT CASE
-           WHEN A + B <= C
-                OR A + C <= B
-                OR B + C <= A THEN 'Not A Triangle'
-           WHEN A = B
-                AND B = C THEN 'Equilateral'
-           WHEN A = B
-                OR A = C
-                OR B = C THEN 'Isosceles'
-           ELSE 'Scalene'
-       END
-FROM TRIANGLES;
+SELECT 
+    CASE 
+        WHEN (A + B <= C OR B + C <= A OR A + C <= B) THEN 'Not A Triangle' 
+        WHEN (A = B AND B = C) THEN 'Equilateral' 
+        WHEN (A != B AND B != C AND C != A) THEN 'Scalene' 
+        ELSE 'Isosceles' 
+    END
+FROM TRIANGLE;
